0b14f27c444349ffa52e6864d7002010424796be
Greg McGhee <gmcghee64@verizon.net>
2018-03-07 09:13:35 -0500

    Initial commit.

---
+++ AcronymBuilder/Acronym.vb
@@ -0,0 +1,23 @@
+﻿
+Public Class Acronym
+    Private _Acronym As String
+    Private _Definition As String
+
+    Public Property Acronym As String
+        Get
+            Return _Acronym
+        End Get
+        Set(value As String)
+            _Acronym = value
+        End Set
+    End Property
+
+    Public Property Definition As String
+        Get
+            Return _Definition
+        End Get
+        Set(value As String)
+            _Definition = value
+        End Set
+    End Property
+End Class
